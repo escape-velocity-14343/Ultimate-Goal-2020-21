ee59d8c180c63284ace44f198f4bf483a84212cc
mihir-jain <mihirrjain200@gmail.com>
2021-04-07 19:31:21 -0700

    Finished creating the turn and drive commands

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainAndPID/TeleOp/AutoStaticRobotPose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainAndPID/TeleOp/AutoStaticRobotPose.java
@@ -1,51 +1,91 @@
 package org.firstinspires.ftc.teamcode.DriveTrainAndPID.TeleOp;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.hardware.RevIMU;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.arcrobotics.ftclib.kinematics.HolonomicOdometry;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.DriveTrainAndPID.EncoderAndPIDDriveTrain;
+import org.firstinspires.ftc.teamcode.DriveTrainAndPID.PIDController;
 
 @TeleOp
 //@Disabled
 public class AutoStaticRobotPose extends LinearOpMode {
 
-    Motor LFMotor, LBMotor, RFMotor, RBMotor;
-    private MotorEx leftEncoder, rightEncoder, perpEncoder;
+    DcMotor LFMotor, LBMotor, RFMotor, RBMotor;
+    private Motor leftEncoder, rightEncoder, perpEncoder;
     private HolonomicOdometry odometry;
-    EncoderAndPIDDriveTrain drive;
     RevIMU imu;
+    FtcDashboard dash = FtcDashboard.getInstance();
+    Telemetry dashboard = dash.getTelemetry();
+    public static double TRACKWIDTH = 15.75;
+    public static double CENTER_WHEEL_OFFSET = 0.5;
+    public static double WHEEL_DIAMETER = 1.49606;
 
-    public static final double TRACKWIDTH = 14.5;
-    public static final double CENTER_WHEEL_OFFSET = 0.5;
-    public static final double WHEEL_DIAMETER = 1.49606;
+    public static double negLeft = 1;
+    public static double negRight = -1;
+    public static double negRear = 1;
+
     // if needed, one can add a gearing term here
-    public static final double TICKS_PER_REV = 360;
+    public static final double TICKS_PER_REV = 360*4;
     public static final double DISTANCE_PER_PULSE = Math.PI * WHEEL_DIAMETER / TICKS_PER_REV;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        leftEncoder = new MotorEx(hardwareMap, "Elevator Motor");
-        rightEncoder = new MotorEx(hardwareMap, "Conveyor Motor");
-        perpEncoder = new MotorEx(hardwareMap, "Right Shooter Motor");
+        leftEncoder = new Motor(hardwareMap, "Elevator Motor");
+        rightEncoder = new Motor(hardwareMap, "Conveyor Motor");
+        perpEncoder = new Motor(hardwareMap, "Right Shooter Motor");
 
-        LFMotor  = new Motor(hardwareMap, "LF Motor", Motor.GoBILDA.RPM_1150);
-        LBMotor  = new Motor(hardwareMap, "LB Motor", Motor.GoBILDA.RPM_1150);
-        RFMotor  = new Motor(hardwareMap, "RF Motor", Motor.GoBILDA.RPM_1150);
-        RBMotor  = new Motor(hardwareMap, "RB Motor", Motor.GoBILDA.RPM_1150);
-
+        LFMotor  = hardwareMap.get(DcMotor.class, "LF Motor");
+        LBMotor  = hardwareMap.get(DcMotor.class, "LB Motor");
+        RFMotor  = hardwareMap.get(DcMotor.class, "RF Motor");
+        RBMotor  = hardwareMap.get(DcMotor.class, "RB Motor");
         imu = new RevIMU(hardwareMap, "imu");
 
-        drive = new EncoderAndPIDDriveTrain(LFMotor, LBMotor, RFMotor, RBMotor, imu);
+        leftEncoder.setDistancePerPulse(negLeft * DISTANCE_PER_PULSE);
+        rightEncoder.setDistancePerPulse(negRight * DISTANCE_PER_PULSE);
+        perpEncoder.setDistancePerPulse(negRear * DISTANCE_PER_PULSE);
 
-        leftEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
-        rightEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
-        perpEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+        LFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        LBMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        RFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        RBMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //setting the motor directions
+        LFMotor.setDirection(DcMotor.Direction.REVERSE);
+        LBMotor.setDirection(DcMotor.Direction.FORWARD);
+        RFMotor.setDirection(DcMotor.Direction.REVERSE);
+        RBMotor.setDirection(DcMotor.Direction.REVERSE);
+
+
+        //defining and creating the IMU sensor for the robot to use
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+
+        LFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        LFMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LBMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RFMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RBMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        imu.init(parameters);
 
         odometry = new HolonomicOdometry(
                 leftEncoder::getDistance,
@@ -57,12 +97,67 @@ public class AutoStaticRobotPose extends LinearOpMode {
 
         waitForStart();
         PositionTracker.robotPose = odometry.getPose();
-        telemetry.addData("X Position in inches: ", PositionTracker.robotPose.getX());
-        telemetry.addData("Y Position in inches: ", PositionTracker.robotPose.getY());
-        telemetry.addData("Rotational Position in degrees: ", PositionTracker.robotPose.getRotation().getDegrees());
-        telemetry.update();
+        dashboard.addData("X Position in inches: ", PositionTracker.robotPose.getX());
+        dashboard.addData("Y Position in inches: ", PositionTracker.robotPose.getY());
+        dashboard.addData("Rotational Position in degrees: ", PositionTracker.robotPose.getRotation().getDegrees());
+        dashboard.update();
 
-        drive.DriveForwardDistance(1, 35);
+        int distance = 35;
+
+        distance /= 2.6;
+
+        LFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //Diameter of wheel = 4in.  Circumference = 12.57; Ticks per revolution of goBilda motor = 1136
+        //Ticks per inch = 1136/12.57 (approximately 90.37)
+        int encoderDistance = (int) ((LFMotor.getCurrentPosition() + RBMotor.getCurrentPosition())/2 + distance * 90);
+
+        //Set target position
+        LFMotor.setTargetPosition(encoderDistance);
+        LBMotor.setTargetPosition(encoderDistance);
+        RFMotor.setTargetPosition(encoderDistance);
+        RBMotor.setTargetPosition(encoderDistance);
+
+        //set run to position mode
+        LFMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        LBMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RFMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RBMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+        LFMotor.setPower(0.25);
+        LBMotor.setPower(0.25);
+        RFMotor.setPower(0.25);
+        RBMotor.setPower(0.25);
+
+
+        while (LFMotor.isBusy() && LBMotor.isBusy() && RFMotor.isBusy() && RBMotor.isBusy()) {
+            odometry.updatePose();
+            PositionTracker.robotPose = odometry.getPose();
+
+            dashboard.addData("X Position in inches: ", PositionTracker.robotPose.getTranslation().getX());
+            dashboard.addData("Y Position in inches: ", PositionTracker.robotPose.getTranslation().getY());
+            dashboard.addData("Rotational Position in degrees: ", PositionTracker.robotPose.getRotation().getDegrees());
+            dashboard.update();
+            LFMotor.setPower(0.25);
+            LBMotor.setPower(0.25);
+            RFMotor.setPower(0.25);
+            RBMotor.setPower(0.25);
+        }
+
+        //Stop and change modes back to normal
+        LFMotor.setPower(0);
+        LBMotor.setPower(0);
+        RFMotor.setPower(0);
+        RBMotor.setPower(0);
+
+        LFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         while (!isStopRequested()) {
             // run autonomous
@@ -71,10 +166,62 @@ public class AutoStaticRobotPose extends LinearOpMode {
             odometry.updatePose();
             PositionTracker.robotPose = odometry.getPose();
 
-            telemetry.addData("X Position in inches: ", PositionTracker.robotPose.getTranslation().getX());
-            telemetry.addData("Y Position in inches: ", PositionTracker.robotPose.getTranslation().getY());
-            telemetry.addData("Rotational Position in degrees: ", PositionTracker.robotPose.getRotation().getDegrees());
-            telemetry.update();
+            dashboard.addData("X Position in inches: ", PositionTracker.robotPose.getTranslation().getX());
+            dashboard.addData("Y Position in inches: ", PositionTracker.robotPose.getTranslation().getY());
+            dashboard.addData("Rotational Position in degrees: ", PositionTracker.robotPose.getRotation().getDegrees());
+            dashboard.update();
+        }
+
+        if (isStopRequested()) {
+            distance = -35;
+
+            distance /= 2.6;
+
+            LFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            LBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            RFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            RBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            //Diameter of wheel = 4in.  Circumference = 12.57; Ticks per revolution of goBilda motor = 1136
+            //Ticks per inch = 1136/12.57 (approximately 90.37)
+            encoderDistance = (int) ((LFMotor.getCurrentPosition() + RBMotor.getCurrentPosition()) / 2 + distance * 90);
+
+            //Set target position
+            LFMotor.setTargetPosition(encoderDistance);
+            LBMotor.setTargetPosition(encoderDistance);
+            RFMotor.setTargetPosition(encoderDistance);
+            RBMotor.setTargetPosition(encoderDistance);
+
+            //set run to position mode
+            LFMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            LBMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            RFMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            RBMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+            LFMotor.setPower(-0.25);
+            LBMotor.setPower(-0.25);
+            RFMotor.setPower(-0.25);
+            RBMotor.setPower(-0.25);
+
+
+            while (LFMotor.isBusy() && LBMotor.isBusy() && RFMotor.isBusy() && RBMotor.isBusy()) {
+                LFMotor.setPower(-0.25);
+                LBMotor.setPower(-0.25);
+                RFMotor.setPower(-0.25);
+                RBMotor.setPower(-0.25);
+            }
+
+            //Stop and change modes back to normal
+            LFMotor.setPower(0);
+            LBMotor.setPower(0);
+            RFMotor.setPower(0);
+            RBMotor.setPower(0);
+
+            LFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            LBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            RFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            RBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         }
     }
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainAndPID/TeleOp/ShooterPIDTesting.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.DriveTrainAndPID.TeleOp;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.DriveTrainAndPID.PIDController;
+
+
+//Back up Auton that goes to the wall side of the bridge, and parks there
+@Config
+@Autonomous (name = "TESTING PID")
+public class ShooterPIDTesting extends LinearOpMode {
+    public static double kP = 0;
+    public static double kI = 0;
+    public static double kD = 0;
+    //initializaing the future variables
+    private ElapsedTime runtime = new ElapsedTime();
+    MotorEx leftShooterMotor, rightShooterMotor;
+
+    //no. of ticks per one revolution of the yellow jacket motors
+    int Ticks_Per_Rev = 1316;
+
+    @Override
+    public void runOpMode(){
+        // Initialize the hardware variables.
+        leftShooterMotor = new MotorEx(hardwareMap, "Left Shooter Motor", Motor.GoBILDA.RPM_1150);
+        rightShooterMotor = new MotorEx(hardwareMap, "Right Shooter Motor", Motor.GoBILDA.RPM_1150);
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        leftShooterMotor.setInverted(false);
+        rightShooterMotor.setInverted(true);
+
+        leftShooterMotor.setRunMode(Motor.RunMode.VelocityControl);
+        rightShooterMotor.setRunMode(Motor.RunMode.VelocityControl);
+
+        leftShooterMotor.setVeloCoefficients(kP, kD, kI);
+        waitForStart();
+
+        //Running the code
+        while (opModeIsActive()) {
+            leftShooterMotor.set(0.8);
+            rightShooterMotor.set(0.8);
+            telemetry.addData("Left Shooter Motor", leftShooterMotor.getVelocity());
+            telemetry.addData("Right Shooter Motor",rightShooterMotor.getVelocity());
+            telemetry.log();
+            telemetry.update();
+        }
+    }
+
+}
